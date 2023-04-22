1d4435552d3e8353ecea8f67e86d5613bed9f8ed
Edson Maia <edsonmaia2005@hotmail.com>
2023-04-22 18:44:08 -0300

    Aula 09
    PageNotFound e ajustes CSS

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Watch from "./pages/Watch";
+import PageNotFound from "./pages/PageNotFound";
 
 function AppRoutes() {
     return (
@@ -8,6 +9,7 @@ function AppRoutes() {
             <Routes>
                 <Route path="/" element={<Home />} ></Route>
                 <Route path="/watch" element={<Watch />} ></Route>
+                <Route path="*" element={<PageNotFound />} ></Route>
             </Routes>
         </BrowserRouter>
     );
