c41feee6523bcf99197c828fe435727a0970eb3b
Edson Maia <edsonmaia2005@hotmail.com>
2023-04-22 17:55:21 -0300

    Aula 07
    Ajustes em Category

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import Banner from "./components/Banner";
 import Card from "./components/Card";
-import Category from "./components/Category";
+import Category, { categories, filterCategory } from "./components/Category";
 import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import videos from "./json/videos.json";
-
-const categories = [
-  "Geografia",
-  "Como fazer e usar",
-  "Astronomia e Geografia",
-  "Climatologia, Meteorologia, Vegetação",
-  "Geologia e Hidrografia"
-]
-
-function filterCategory(id) {
-  return videos.filter( video => video.category === categories[id] )
-}
 
 function App() {
 
@@ -26,25 +13,11 @@ function App() {
       <Banner image="favoritos" />
       <Container>
 
-        <Category category={categories[0]}>
-          { filterCategory(0).map((video) => <Card id={video.id} key={video.id} /> )}
-        </Category>
-
-        <Category category={categories[1]}>
-          { filterCategory(1).map((video) => <Card id={video.id} key={video.id} /> )}
-        </Category>
-
-        <Category category={categories[2]}>
-          { filterCategory(2).map((video) => <Card id={video.id} key={video.id} /> )}
-        </Category>
-
-        <Category category={categories[3]}>
-          { filterCategory(3).map((video) => <Card id={video.id} key={video.id} /> )}
-        </Category>
-
-        <Category category={categories[4]}>
-          { filterCategory(4).map((video) => <Card id={video.id} key={video.id} /> )}
-        </Category>
+      { categories.map((category, index) => 
+          <Category category={category} key={index} >
+              { filterCategory(index).map(video => <Card id={video.id} key={video.id} /> )}
+          </Category>
+      )}
 
       </Container>
       <Footer />
